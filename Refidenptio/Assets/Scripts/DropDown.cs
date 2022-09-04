2977157ac5b7ad879dfddaf4a7c12b9347c881dd
Thaliston679 <90582107+Thaliston679@users.noreply.github.com>
2022-09-04 00:29:12 -0300

    Menu Config

---
+++ Refidenptio/Assets/Scripts/DropDown.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Rendering;
+
+public class DropDown : MonoBehaviour
+{
+    public RenderPipelineAsset[] qualityLevels;
+    public TMP_Dropdown dropDown;
+    
+    void Start()
+    {
+        dropDown.value = QualitySettings.GetQualityLevel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeLevel(int value)
+    {
+        QualitySettings.SetQualityLevel(value);
+        QualitySettings.renderPipeline = qualityLevels[value];
+    }
+
+    public void ChangeResolution(int value)
+    {
+        switch (value) 
+        {
+            case 0:
+                Resolution1920();
+                break;
+            case 1:
+                Resolution1280();
+                break;
+            case 2:
+                Resolution720();
+                break;
+        }
+    }
+
+    public void Resolution1920()
+    {
+        Screen.SetResolution(1920, 1080, true);
+    }
+
+    public void Resolution1280()
+    {
+        Screen.SetResolution(1280, 720, true);
+    }
+
+    public void Resolution720()
+    {
+        Screen.SetResolution(720, 480, true);
+    }
+}
